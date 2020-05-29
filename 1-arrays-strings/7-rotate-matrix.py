b4a217edbe2da32c5aa7f3adb4006e564f6d33aa
Kevin Chen <kevin.chen.krc@gmail.com>
2020-05-28 20:50:52 -0400

    Require fewer function arguments

--- 1-arrays-strings/7-rotate-matrix.py
+++ 1-arrays-strings/7-rotate-matrix.py
@@ -8,36 +8,40 @@ from typing import List
 
 def main():
     m = [[1]]
-    rotate_matrix(m, 1)
+    rotate_matrix(m)
     print_matrix(m)
     m2 = [
         [1, 2], 
         [3, 4]
     ]
-    rotate_matrix(m2, 2)
+    rotate_matrix(m2)
     print_matrix(m2)
     m3 = [
         [1, 2, 3], 
         [4, 5, 6], 
         [7, 8, 9]
     ]
-    rotate_matrix(m3, 3)
+    rotate_matrix(m3)
     print_matrix(m3)
 
 
-def rotate_matrix(m: List[List[int]], n: int):
+def rotate_matrix(mat: List[List[int]]):
+    n = len(mat)
+    # rotate each square "layer", starting from outer layer and moving inward
     for i in range(n // 2):
         for j in range(n - 2 * i):
+            # four-way swap matrix entries in layer in-place
             coords = [(i, j)]
             for k in range(1, 4):
                 coords.append(rotate_coord(coords[k - 1][0], coords[k - 1][1], n))
-            temp = m[coords[3][0]][coords[3][1]]
+            temp = mat[coords[3][0]][coords[3][1]]
             for k in reversed(range(1, 4)):
-                m[coords[k][0]][coords[k][1]] = m[coords[k - 1][0]][coords[k - 1][1]]
-            m[coords[0][0]][coords[0][1]] = temp
+                mat[coords[k][0]][coords[k][1]] = mat[coords[k - 1][0]][coords[k - 1][1]]
+            mat[coords[0][0]][coords[0][1]] = temp
 
 
 def rotate_coord(x: int, y: int, n: int) -> (int, int):
+    # find counterclockwise rotated coordinates in n-by-n matrix
     center = (n - 1) / 2
     x -= center
     y -= center
