8fce3935837ecb78e71c7a12cdd29eb188373348
Kevin Chen <kevin.chen.krc@gmail.com>
2020-05-31 20:21:43 -0400

    Finish queue via stacks

---
+++ 3-stacks-queues/4-queue-via-stacks.py
@@ -0,0 +1,72 @@
+# Queue via Stacks
+# Implement a MyQueue class which implements a queue using two stacks.
+
+from queue import AbstractQueue, test_queue
+from stack import Stack
+import unittest
+
+
+class MyQueue(AbstractQueue):
+    def __init__(self, arr=None):
+        self.addstack = Stack()
+        self.removestack = Stack()
+        if arr is not None:
+            for val in arr:
+                self.add(val)
+
+    def __iter__(self):
+        for val in self.removestack:
+            yield val
+        added = []
+        for val in self.addstack:
+            added.append(val)
+        for val in reversed(added):
+            yield val
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        """Returns space-separated string of values in queue, front to back"""
+        vals = []
+        for val in self:
+            vals.append(str(val))
+        return " ".join(vals)
+
+    def add(self, val):
+        """Adds `val` to end of queue"""
+        self.addstack.push(val)
+
+    def remove(self):
+        """Removes and returns first value in queue"""
+        if self.isempty():
+            raise Exception("Called remove on empty queue")
+        if self.removestack.isempty():
+            self.push_add_to_remove()
+        return self.removestack.pop()
+
+    def peek(self):
+        """Returns first value in queue without removing"""
+        if self.isempty():
+            raise Exception("Called peek on empty queue")
+        if self.removestack.isempty():
+            self.push_add_to_remove()
+        return self.removestack.peek()
+
+    def isempty(self) -> bool:
+        """Returns true iff queue is empty"""
+        return self.addstack.isempty() and self.removestack.isempty()
+
+    def push_add_to_remove(self):
+        """Moves all elements in `addstack` to `removestack`, inverting order"""
+        while not self.addstack.isempty():
+            val = self.addstack.pop()
+            self.removestack.push(val)
+
+
+class TestMyQueue(test_queue(lambda x=None: MyQueue(x))):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
