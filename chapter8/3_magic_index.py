60a806f2b12b0d4df41c76e9d623dbd820e322af
Kevin Chen <kevin.chen.krc@gmail.com>
2020-06-04 21:05:01 -0400

    Finish magic index

---
+++ chapter8/3_magic_index.py
@@ -0,0 +1,56 @@
+# Magic Index
+# A magic index in an array A[0 ... n-1] is defined to be an index such that
+# A[i] = i. Given a sorted array of distinct integers, write a method to find
+# a magic index, if one exists, in array A.
+#
+# FOLLOW UP
+# What if the values are not distinct?
+
+from typing import List
+import unittest
+
+# where mid = (i+j)//2,
+# f(arr, i, j) =    None,               i >= j
+#                   mid,                arr[mid] == mid
+#                   f(arr, mid+1, j),   arr[mid] < mid
+#                   f(arr, i, mid),     otherwise
+
+
+def magic_index(arr: List[int], i: int = 0, j: int = None) -> int:
+    if j is None:
+        j = len(arr)
+    if i >= j:
+        return None
+    mid = (i + j) // 2
+    if arr[mid] == mid:
+        return mid
+    elif arr[mid] < mid:
+        return magic_index(arr, mid+1, j)
+    else:
+        return magic_index(arr, i, mid)
+
+
+class TestMagicIndex(unittest.TestCase):
+    def test_magic_index(self):
+        trues = [
+            # array, magic indices
+            [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4]],
+            [[-5, -3, 2, 5, 7], [2]],
+            [[0, 4, 7, 9], [0]],
+            [[-9, -5, 1, 3, 4], [3, 4]]
+        ]
+        falses = [
+            [1, 2, 3, 4, 5],
+            [-5, -3, 3, 4],
+            [-9, -5, 1, 2, 3]
+        ]
+        for arr, indices in trues:
+            with self.subTest(arr=arr):
+                self.assertIn(magic_index(arr), indices)
+        for arr in falses:
+            with self.subTest(arr=arr):
+                self.assertIsNone(magic_index(arr))
+
+
+if __name__ == "__main__":
+    unittest.main()
